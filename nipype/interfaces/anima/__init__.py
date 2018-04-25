75fa6886ee7c72825e162219da3a0f06f98435b4
Mikaël Naveau <Mikal Naveau>
2018-04-25 23:01:42 +0200

    Add CropImage interface

--- nipype/interfaces/anima/__init__.py
+++ nipype/interfaces/anima/__init__.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
-"""Top-level namespace for anima."""
+"""
+Nipype Anima interfaces
+----------------------
+
+This module defines the API of Anima interfaces.
+"""
 
 # Registration stuff
 from .registration import (PyramidalBMRegistration)
@@ -10,4 +15,8 @@ from .registration import (PyramidalBMRegistration)
 from .filtering import (NLMeans)
 
 # Utility stuff
-from .utils import (ImageArithmetic, AverageImages)
+from .utils import (
+    CropImage,
+    ImageArithmetic,
+    AverageImages
+)
