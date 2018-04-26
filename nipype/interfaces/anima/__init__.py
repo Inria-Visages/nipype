8d7aa2a5958736819f4cdf7cf43344b551419713
Olivier Commowick <olivier.commowick@inria.fr>
2018-04-26 11:11:46 +0200

    Start filling init file

---
+++ nipype/interfaces/anima/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+"""Top-level namespace for anima."""
+
+# Registration stuff
+from .registration import (PyramidalBMRegistration)
+
+# Filtering stuff
+from .filtering import (NLMeans)
