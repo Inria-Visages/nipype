c83b7ca24d6312bfcba2538e18366e137d92a4dd
Olivier Commowick <olivier.commowick@inria.fr>
2018-04-30 14:57:34 +0200

    Add transform series to registration to handle XML genration

--- nipype/interfaces/anima/registration.py
+++ nipype/interfaces/anima/registration.py
@@ -25,19 +25,25 @@ class PyramidalBMRegistrationInputSpec(CommandLineInputSpec):
                        desc='moving image')
     out_file = File(argstr='-o %s', desc='output (registered) image',
                     mandatory=True)
+    input_transform_file = File(argstr='-i %s',
+                                desc='input transformation matrix (ITK txt format)')
     out_transform_file = File(argstr='-O %s',
-                              desc='output affine matrix (ITK txt format)')
+                              desc='output transformation matrix (ITK txt format)')
     out_transform_type = traits.Enum(0, 1, 2, argstr='--ot %d',
                                      usedefault=True,
                                      desc='output transformation type (0: rigid, 1: translation, 2: affine)')
+
+    input_transform_series = traits.List('', usedefault=True,
+                                         desc='Input transformations list (previous transformations applied)')
+
     metric_type = traits.Enum(0, 1, 2, argstr='--metric %d',
                               usedefault=True,
                               desc='Similarity metric between blocks (0: squared correlation coefficient, \
                                     1: correlation coefficient, 2: mean squares')
-    blocks_spacing = traits.Int(5, argstr='--sp %d', usedefault=True,
-                                desc='block spacing')
-    blocks_search_radius = traits.Int(2, argstr='--sr %d', usedefault=True,
-                                      desc='Search radius in pixels (exhaustive search window, rho start for bobyqa')
+    block_spacing = traits.Int(5, argstr='--sp %d', usedefault=True,
+                               desc='block spacing')
+    block_search_radius = traits.Int(2, argstr='--sr %d', usedefault=True,
+                                     desc='Search radius in pixels (exhaustive search window, rho start for bobyqa')
     pyramid_levels = traits.Int(3, argstr="-p %d", usedefault=True,
                                 desc='number of pyramid levels')
     last_pyramid_level = traits.Int(0, argstr='-l %d', usedefault=True,
@@ -49,7 +55,8 @@ class PyramidalBMRegistrationInputSpec(CommandLineInputSpec):
 class PyramidalBMRegistrationOutputSpec(TraitedSpec):
     out_file = File(exists=True, desc='output (registered) image')
     out_transform_file = File(exists=True, desc='output affine matrix (ITK txt format)')
-
+    out_transform_series = traits.List('', usedefault=True,
+                                       desc='Output transformations list (input followed by current')
 
 class PyramidalBMRegistration(CommandLine):
     _cmd = 'animaPyramidalBMRegistration'
@@ -60,8 +67,10 @@ class PyramidalBMRegistration(CommandLine):
         outputs = self.output_spec().get()
         outputs['out_file'] = os.path.abspath(self.inputs.out_file)
 
+        outputs['out_transform_series'] = self.inputs.input_transform_series
         if self.inputs.out_transform_file:
             outputs['out_transform_file'] = os.path.abspath(self.inputs.out_transform_file)
+            outputs['out_transform_series'].append(os.path.abspath(self.inputs.out_transform_file))
 
         return outputs
 
@@ -75,14 +84,18 @@ class DenseSVFBMRegistrationInputSpec(CommandLineInputSpec):
                     mandatory=True)
     out_transform_file = File(argstr='-O %s',
                               desc='output SVF transform')
+
+    input_transform_series = traits.List('', usedefault=True,
+                                         desc='Input transformations list (previous transformations applied)')
+
     metric_type = traits.Enum(0, 1, 2, argstr='--metric %d',
                               usedefault=True,
                               desc='Similarity metric between blocks (0: squared correlation coefficient, \
                                     1: correlation coefficient, 2: mean squares')
-    blocks_spacing = traits.Int(2, argstr='--sp %d', usedefault=True,
-                                desc='block spacing')
-    blocks_search_radius = traits.Int(2, argstr='--sr %d', usedefault=True,
-                                      desc='Search radius in pixels (exhaustive search window, rho start for bobyqa')
+    block_spacing = traits.Int(2, argstr='--sp %d', usedefault=True,
+                               desc='block spacing')
+    block_search_radius = traits.Int(2, argstr='--sr %d', usedefault=True,
+                                     desc='Search radius in pixels (exhaustive search window, rho start for bobyqa')
     pyramid_levels = traits.Int(3, argstr="-p %d", usedefault=True,
                                 desc='number of pyramid levels')
     last_pyramid_level = traits.Int(0, argstr='-l %d', usedefault=True,
@@ -94,6 +107,8 @@ class DenseSVFBMRegistrationInputSpec(CommandLineInputSpec):
 class DenseSVFBMRegistrationOutputSpec(TraitedSpec):
     out_file = File(exists=True, desc='output (registered) image')
     out_transform_file = File(exists=True, desc='output SVF transform')
+    out_transform_series = traits.List('', usedefault=True,
+                                       desc='Output transformations list (input followed by current')
 
 
 class DenseSVFBMRegistration(CommandLine):
@@ -105,8 +120,10 @@ class DenseSVFBMRegistration(CommandLine):
         outputs = self.output_spec().get()
         outputs['out_file'] = os.path.abspath(self.inputs.out_file)
 
+        outputs['out_transform_series'] = self.inputs.input_transform_series
         if self.inputs.out_transform_file:
             outputs['out_transform_file'] = os.path.abspath(self.inputs.out_transform_file)
+            outputs['out_transform_series'].append(os.path.abspath(self.inputs.out_transform_file))
 
         return outputs
 
@@ -116,7 +133,7 @@ class TransformSerieXmlGeneratorInputSpec(CommandLineInputSpec):
                                  desc='input transformations')
     input_inversion_flags = traits.ListInt(argstr='-I %s', sep=' -I ',
                                            desc='transformations inversion flags')
-    dense_trsf_flag = traits.Bool(argstr='-D', desc='non linear transfroms are dense fields')
+    dense_trsf_flag = traits.Bool(argstr='-D', desc='non linear transforms are dense fields')
     out_file = File(argstr='-o %s', desc='output XML file',
                     mandatory=True)
 
@@ -147,11 +164,10 @@ class ApplyTransformSerieInputSpec(CommandLineInputSpec):
                          desc='geometry image')
     interpolation_mode = traits.Enum('linear', 'nearest', 'bspline', 'sinc',
                                      argstr='-n %s', usedefault=True,
-                                     desc='interpolation mode (one of nearest, linear, bspline, sinc, \
-                                           default is linear)')
+                                     desc='interpolation mode (one of nearest, linear, bspline, sinc)')
 
-    number_of_threads = traits.Enum(0, argstr='-T %d', usedefault=True,
-                                   desc='number of threads to run on')
+    number_of_threads = traits.Enum(0, argstr='-p %d', usedefault=True,
+                                    desc='number of threads to run on')
 
 
 class ApplyTransformSerieOutputSpec(TraitedSpec):
